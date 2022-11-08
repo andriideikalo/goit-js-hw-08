3d15d6cd7a82092bb70f9aca2e7485c19551c630
andriideikalo <wlada801@gmail.com>
2022-11-08 11:47:47 +0200

    hw task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+// console.log(SimpleLightbox);
+
+
+const imgGalery = document.querySelector(`.gallery`);
+const markup = galleryItems.map(({ preview, original, description }) =>
+    `<a href="${original}" class = "galery__link" >
+    <img src="${preview}" data-src="${original}"alt="${description}" class = "gallery__image" width ="340">
+     </a>`).join(``);
+// console.log(markup);
+imgGalery.insertAdjacentHTML(`beforeend`, markup);
+
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function() {
+    // Do something…
+});
+
 // Change code below this line
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,5 @@
+import SimpleLightbox from "simplelightbox";
+import Player from '@vimeo/player';
 
+console.log(SimpleLightbox);
+console.log(Player);
